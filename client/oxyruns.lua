861c360b0f14fd24f42ad7feff455f0019b6ecd4
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-12-20 00:07:58 -0500

    Update oxyruns.lua

--- client/oxyruns.lua
+++ client/oxyruns.lua
@@ -12,15 +12,19 @@ if TriggerServerEvent('md-drugs:server:payfortruck') then
 end
 end)
 
-function PolicecallOxy()
-print "Tell Your Dev They Didn't Follow The Readme"
+function Policecall()
+	exports['ps-dispatch']:DrugSale()
 end
 
 
 RegisterNetEvent("md-drugs:Client:getoxylocation", function()
 	local coords = Config.truckspawn
 	local ModelHash = "burrito3" -- Use Compile-time hashes to get the hash of this model
-	lib.requestModel(ModelHash, 500)
+	if not IsModelInCdimage(ModelHash) then return end
+	RequestModel(ModelHash) -- Request the model
+	while not HasModelLoaded(ModelHash) do -- Waits for the model to load
+	Wait(0)
+	end
 	local oxycar = CreateVehicle(ModelHash,Config.truckspawn.x, Config.truckspawn.y,Config.truckspawn.z, Config.truckspawn.h, true, false)
     SetEntityHeading(oxycar, coords.w)
     exports[Config.Fuel]:SetFuel(oxycar, 100.0)
@@ -28,18 +32,62 @@ RegisterNetEvent("md-drugs:Client:getoxylocation", function()
 	TriggerEvent('QBCore:Notify', 'Your truck is here', 'success')
 	TriggerEvent("md-drugs:client:getoxylocationroute")
     SetVehicleEngineOn(oxycar, true, true)
-	 exports['qb-target']:AddTargetEntity(oxycar, {
+	exports['qb-target']:AddTargetEntity(oxycar, {
     options = {
         {
+            type = "client",
             event = "md-drugs:client:getfromtrunk",
             icon = "fas fa-box-circle-check",
             label = "Get Package",
+            
         },
-    }
-	})
+    },
+    distance = 3.0
+})
 end)
 
-
+CreateThread(function()
+ if Config.oxtarget then
+ exports.ox_target:addBoxZone({ -- 963.37, -2122.95, 31.47
+		coords = Config.Payfortruck,
+		size = vec3(2,2,2),
+		debugPoly = false,
+		rotation = 45,
+		options = {
+			{
+            type = "client",
+            event = "md-drugs:client:getoxytruck",
+			icon = "fas fa-sign-in-alt",
+			label = "Pay For Truck",
+			
+			
+			},
+		},
+	})
+ else
+ exports['qb-target']:AddBoxZone("getoxylocation",Config.Payfortruck,1.5, 1.75, { -- 963.37, -2122.95, 31.47
+	name = "getoxylocation",
+	heading = 11.0,
+	debugPoly = false,
+	minZ = Config.Payfortruck-2,
+	maxZ = Config.Payfortruck+2,
+}, {
+	options = {
+		{
+            type = "client",
+            event = "md-drugs:client:getoxytruck",
+			icon = "fas fa-sign-in-alt",
+			label = "Pay For Truck",
+			
+			
+		},
+		
+	},
+	distance = 2.5
+ })
+	
+end	
+end)
 RegisterNetEvent("md-drugs:client:getoxylocationroute")
 AddEventHandler("md-drugs:client:getoxylocationroute", function() 
 	Wait(100)
@@ -60,33 +108,35 @@ AddEventHandler("md-drugs:client:getoxylocationroute", function()
     AddTextComponentSubstringPlayerName("drug Meet")
     EndTextCommandSetBlipName(deliveryBlip)
     SetBlipRoute(deliveryBlip, true)
-	oxyzone = lib.zones.sphere({
-		coords = CurrentLocation,
-		radius = 2,
-		onEnter = function()
-			RemoveBlip(deliveryBlip)
-			end,
-		})
+	druglocation = CircleZone:Create(CurrentLocation, 5,{ name = "oxyruns", debugPoly = false })
+	druglocation:onPlayerInOut(function(isPointInside) if isPointInside then  RemoveBlip(deliveryBlip) end end)
 	end
-	if unlucky <= Config.PoliceAlertOxy then 
-        PolicecallOxy()
+	if unluck <= Config.PoliceAlertOxy then 
+        Policecall()
     end
 	local current = "g_m_y_famdnf_01"
-    lib.requestModel(current, 500)
-    oxybuyer = CreatePed(0, current,CurrentLocation.x,CurrentLocation.y,CurrentLocation.z-1, false, false)
-	SetEntityHeading(oxybuyer, 180)
-     FreezeEntityPosition(oxybuyer, true)
-    SetEntityInvincible(oxybuyer, true)
-	exports['qb-target']:AddTargetEntity(oxybuyer, { 
-        options = {
-            {
-                type = "client",
-                label = "Talk To Buyer",
-                icon = "fas fa-eye",
-                event = "md-drugs:client:giveoxybox"
-            },
-        }
-     })
+            RequestModel(current)
+            while not HasModelLoaded(current) do
+                Wait(0)
+            end
+            
+            drugdealer = CreatePed(0, current,CurrentLocation.x,CurrentLocation.y,CurrentLocation.z-1, false, false)
+			SetEntityHeading(drugdealer, 180)
+             FreezeEntityPosition(drugdealer, true)
+            SetEntityInvincible(drugdealer, true)
+	exports['qb-target']:AddTargetEntity(drugdealer, {
+                options = {
+                    {
+                        type = "client",
+                        label = "Talk To Buyer",
+                        icon = "fas fa-eye",
+                        event = "md-drugs:client:giveoxybox"
+                    },
+                },
+                distance = 2.0
+            })
+	 
+	
 end)
 
 
@@ -123,8 +173,10 @@ AddEventHandler("md-drugs:client:giveoxybox", function()
 			disableInventory = true,
 		}, {}, {}, {}, function()-- Done
 			TriggerServerEvent("md-drugs:server:giveoxybox")
+			
 			ClearPedTasks(PlayerPedId())
-			DeleteEntity(oxybuyer)
+			
+			DeleteEntity(drugdealer)
 			 DetachEntity(carryPackage, true, true)
 			 DeleteObject(carryPackage)
 			 carryPackage = nil
